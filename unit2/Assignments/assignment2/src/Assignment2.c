d6f2325fe5a07c3825869097f45aeaa6135d98dc
abdo adel <abdooadel14@gmail.com>
2023-09-30 13:10:53 +0100

    ass2 ex2 solved

--- unit2/Assignments/assignment2/src/Assignment2.c
+++ unit2/Assignments/assignment2/src/Assignment2.c
@@ -26,20 +26,37 @@ int main(void) {
 	//		printf("%d is odd",x);
 
 	//Ex2 check vower or consonant
+	//
+	//	char try;
+	//	printf("\nEnter an alphabet: ");
+	//	fflush(stdin);
+	//	fflush(stdout);
+	//	scanf("%c",&try);
+	//	if(try=='i' || try=='a' || try=='e' || try=='o' ||
+	//		try=='I' || try=='A' || try=='E' || try=='O')
+	//		printf("\n%c is a vowel",try);
+	//	else
+	//		printf("\n%c is a consonant",try);
 
-	char try;
-	printf("\nEnter an alphabet: ");
+	// Ex3  find biggest of three numbers
+	float num1,num2,num3,big_num;
+	printf("Enter three numbers: ");
 	fflush(stdin);
 	fflush(stdout);
-	scanf("%c",&try);
-	if(try=='i' || try=='a' || try=='e' || try=='o' ||
-		try=='I' || try=='A' || try=='E' || try=='O')
-		printf("\n%c is a vowel",try);
+	scanf("%f %f %f",&num1,&num2,&num3);
+	if(num1>num2 && num1>num3)
+		big_num=num1;
+	else if (num2>num1 &&num2>num3)
+		big_num=num2;
 	else
-		printf("\n%c is a consonant",try);
+		big_num=num3;
+	printf("Largest number = %f",big_num);
 
 
 
 
-	return EXIT_SUCCESS;
+
+
+
+	return 0;
 }
