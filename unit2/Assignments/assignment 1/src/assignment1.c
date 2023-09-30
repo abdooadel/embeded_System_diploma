073b5b3a0bf7f8265839042e7e0d1f44ff5b7968
abdo adel <abdooadel14@gmail.com>
2023-09-30 04:01:51 +0100

    Ex1 solved

---
+++ unit2/Assignments/assignment 1/src/assignment1.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : assignment.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	//Ex1
+
+	printf("C Programming");
+
+
+	return EXIT_SUCCESS;
+}
