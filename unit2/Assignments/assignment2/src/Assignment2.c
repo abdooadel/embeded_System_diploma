f193c9814e5b22134e876d10cd7aee3febe5f47d
abdo adel <abdooadel14@gmail.com>
2023-09-30 13:40:19 +0100

    ass2 all Ex solved

--- unit2/Assignments/assignment2/src/Assignment2.c
+++ unit2/Assignments/assignment2/src/Assignment2.c
@@ -15,71 +15,71 @@ int main(void) {
 
 	// Ex1 check even or odd
 
-	//	int x;
-	//	printf("Enter an integer you want to check: ");
-	//	fflush(stdin);
-	//	fflush(stdout);
-	//	scanf("%d",&x);
-	//	if(x%2==0)
-	//		printf("%d is even",x);
-	//	else
-	//		printf("%d is odd",x);
+		int x;
+		printf("Enter an integer you want to check: ");
+		fflush(stdin);
+		fflush(stdout);
+		scanf("%d",&x);
+		if(x%2==0)
+			printf("%d is even",x);
+		else
+			printf("%d is odd",x);
 
 	//Ex2 check vower or consonant
-	//
-	//	char try;
-	//	printf("\nEnter an alphabet: ");
-	//	fflush(stdin);
-	//	fflush(stdout);
-	//	scanf("%c",&try);
-	//	if(try=='i' || try=='a' || try=='e' || try=='o' ||
-	//		try=='I' || try=='A' || try=='E' || try=='O')
-	//		printf("\n%c is a vowel",try);
-	//	else
-	//		printf("\n%c is a consonant",try);
+
+		char try;
+		printf("\nEnter an alphabet: ");
+		fflush(stdin);
+		fflush(stdout);
+		scanf("%c",&try);
+		if(try=='i' || try=='a' || try=='e' || try=='o' ||
+			try=='I' || try=='A' || try=='E' || try=='O')
+			printf("\n%c is a vowel",try);
+		else
+			printf("\n%c is a consonant",try);
 
 	// Ex3  find biggest of three numbers
-	//	float num1,num2,num3,big_num;
-	//	printf("Enter three numbers: ");
-	//	fflush(stdin);
-	//	fflush(stdout);
-	//	scanf("%f %f %f",&num1,&num2,&num3);
-	//	if(num1>num2 && num1>num3)
-	//		big_num=num1;
-	//	else if (num2>num1 &&num2>num3)
-	//		big_num=num2;
-	//	else
-	//		big_num=num3;
-	//	printf("Largest number = %f",big_num);
+		float num1,num2,num3,big_num;
+		printf("Enter three numbers: ");
+		fflush(stdin);
+		fflush(stdout);
+		scanf("%f %f %f",&num1,&num2,&num3);
+		if(num1>num2 && num1>num3)
+			big_num=num1;
+		else if (num2>num1 &&num2>num3)
+			big_num=num2;
+		else
+			big_num=num3;
+		printf("Largest number = %f",big_num);
 
 
 	//Ex5 check alphabet
 
-	//	char alpha;
-	//	printf("Enter a character: ");
-	//	fflush(stdin);
-	//	fflush(stdout);
-	//	scanf("%c",&alpha);
-	//	if ((alpha >= 65 && alpha<91) || (alpha >= 97 && alpha< 122) )
-	//		printf("%c is an alphabet",alpha);
-	//	else
-	//		printf("%c is not an alphabet",alpha);
+		char alpha;
+		printf("Enter a character: ");
+		fflush(stdin);
+		fflush(stdout);
+		scanf("%c",&alpha);
+		if ((alpha >= 65 && alpha<91) || (alpha >= 97 && alpha< 122) )
+			printf("%c is an alphabet",alpha);
+		else
+			printf("%c is not an alphabet",alpha);
 
-	//
+
 	//
 	//	//Ex6 sum of numbers
 	//
-	//	int n,summ;
-	//	printf("Enter an integer: ");
-	//	fflush(stdin);
-	//	fflush(stdout);
-	//	scanf("%d",&n);
-	//	for(int i=0;i<=n;i++)
-	//	{
-	//		summ+=i;
-	//	}
-	//	printf("Sum = %d",summ);
-	//
+		int n,summ;
+		printf("Enter an integer: ");
+		fflush(stdin);
+		fflush(stdout);
+		scanf("%d",&n);
+		for(int i=0;i<=n;i++)
+		{
+			summ+=i;
+		}
+		printf("Sum = %d",summ);
+
 
 
 	//Ex8 Calculator
