c3fec340083f8d7b6a4a9dbcce6be78b73517eaf
abdo adel <abdooadel14@gmail.com>
2023-09-30 04:16:38 +0100

    Ex5 solved

--- unit2/Assignments/assignment 1/src/assignment1.c
+++ unit2/Assignments/assignment 1/src/assignment1.c
@@ -13,33 +13,42 @@
 
 int main(void) {
 
-	//Ex1
+//	//Ex1
+//
+//	printf("C Programming");
+//
+//	//Ex2
+//	int num;
+//	printf("\nEnter a integer: ");
+//	fflush(stdin);
+//	fflush(stdout);
+//	scanf("%d",&num);
+//	printf("\nYou entered: %d",num);
+//
+//	//Ex3
+//	int num1,num2;
+//	printf("\nEnter two integer: ");
+//	fflush(stdin);
+//	fflush(stdout);
+//	scanf("%d %d",&num1,&num2);
+//	printf("\nSum:  %d",num1+num2);
+//
+//	//Ex4
+//	float num11,num22;
+//	printf("\nEnter two Floating Point Numbers: ");
+//	fflush(stdin);
+//	fflush(stdout);
+//	scanf("%f %f",&num11,&num22);
+//	printf("\nSum:  %f",num11*num22);
 
-	printf("C Programming");
-
-	//Ex2
-	int num;
-	printf("\nEnter a integer: ");
+	//Ex5
+	char input;
+	printf("Enter a character: ");
 	fflush(stdin);
 	fflush(stdout);
-	scanf("%d",&num);
-	printf("\nYou entered: %d",num);
+	scanf("%c",&input);
+	printf("ASCII value of %c = %d",input,input);
 
-	//Ex3
-	int num1,num2;
-	printf("\nEnter two integer: ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d %d",&num1,&num2);
-	printf("\nSum:  %d",num1+num2);
-
-	//Ex4
-	float num11,num22;
-	printf("\nEnter two Floating Point Numbers: ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%f %f",&num11,&num22);
-	printf("\nSum:  %f",num11*num22);
 
 
 	return EXIT_SUCCESS;
